d036b0b4edaa7e6bcda0801d5cf05e864faa4ea7
RobinLind <robin_flaks@hotmail.com>
2017-10-14 19:35:47 +0100

    project fix

--- SnakeMess/check.cs
+++ SnakeMess/check.cs
@@ -1,41 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace SnakeMess
 {
-    public class Check
+    class check
     {
-        public Check()
-        {
-        }
-
-        public static Boolean gg(int newPosX, int newPosY, int snakeLength, int boardH, int boardW)
-        {
-            if (newPosX < 0 || newPosX >= boardW)
-                return true;
-            if (newPosY < 0 || newPosY >= boardH)
-                return true;
-            if (snakeLength + 1 >= boardW * boardH)
+            public check()
             {
-                // No more room to place apples - game over.
-                return true;
             }
 
-            return false;
-        }
-
-        public static Boolean canibal(List<Point> snake, int newPosX, int newPosY)
-        {
-            foreach (Point x in snake)
-                if (x.X == newPosX && x.Y == newPosY)
+            public static Boolean gg(int newPosX, int newPosY, int snakeLength, int boardH, int boardW)
+            {
+                if (newPosX < 0 || newPosX >= boardW)
+                    return true;
+                if (newPosY < 0 || newPosY >= boardH)
+                    return true;
+                if (snakeLength + 1 >= boardW * boardH)
                 {
-                    // Death by accidental self-cannibalism.
+                    // No more room to place apples - game over.
                     return true;
                 }
-            return false;
+
+                return false;
+            }
+
+            public static Boolean canibal(List<Point> snake, int newPosX, int newPosY)
+            {
+                foreach (Point x in snake)
+                    if (x.X == newPosX && x.Y == newPosY)
+                    {
+                        // Death by accidental self-cannibalism.
+                        return true;
+                    }
+                return false;
+
+            }
+
 
         }
-
-            
     }
-}
+
