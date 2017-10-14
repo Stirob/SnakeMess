50ac7ddb14f84929c4877b45610fc1e2d5323aee
Stirob <itsbatterydrinker@hotmail.com>
2017-10-14 18:00:38 +0200

    Segment
    far faktisk mye vanskeligere å sette seg inn i annen kode en det jeg
    hadde trodd lol

---
+++ SnakeMess/Segment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnakeMess
+{
+    class Segment
+    {
+        var rng = new Random();
+        var app = new Point();
+        List<Point> snake = new List<Point>();
+        snake.Add(new Point(10, 10));
+		snake.Add(new Point(10, 10));
+		snake.Add(new Point(10, 10));
+		snake.Add(new Point(10, 10));
+    }
+
+    while (true) {
+				app.X = rng.Next(0, boardW);
+				app.Y = rng.Next(0, boardH);
+
+				bool spot = true;
+
+				foreach (Point i in snake)
+					if (i.X == app.X && i.Y == app.Y) {
+						spot = false;
+						break;
+					}
+					if (spot) {
+					Console.ForegroundColor = ConsoleColor.Green; Console.SetCursorPosition(app.X, app.Y); Console.Write("$");
+					break;
+				}
+}
